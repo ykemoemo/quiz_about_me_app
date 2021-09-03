a16aa3ee68ec4834271bf4392a5e4044e57c5329
ykemoemo <ykemoemo@gmail.com>
2021-09-03 20:03:55 +0900

    Update gemfile.lock for heroku

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module QuizAboutMeApp
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
   end
 end
