13fc56af8140a3e86cb18ed18f4e0fa9639347f5
ykemoemo <ykemoemo@gmail.com>
2021-09-18 11:47:39 +0900

    Update 1

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -3,27 +3,27 @@ class ChoicesController < ApplicationController
   before_action :set_question
 
   def set_correct_answer
-    @choice = @question.choices.find(params[:id])
-    if @choice.correct_answer == true
-      @choice.correct_answer = false
+    choice = @question.choices.find(params[:id])
+    if choice.correct_answer == true
+      choice.correct_answer = false
     else
-      @choice.correct_answer = true
+      choice.correct_answer = true
     end
-    @choice.save
+    choice.save
     @questions = @quiz.questions.all.includes(:quiz)
   end
 
   def judgement
-    @challenger = @quiz.challengers.find(params[:challenger_id])
-    @choice = @question.choices.find(params[:id])
-    if @choice.correct_answer == true
-      @challenger.score += 1
-      @challenger.save
+    challenger = @quiz.challengers.find(params[:challenger_id])
+    choice = @question.choices.find(params[:id])
+    if choice.correct_answer == true
+      challenger.score += 1
+      challenger.save
       flash[:notice] = "正解！！"
-      redirect_to quiz_challenger_question_path(@quiz.id, @challenger.id, @question.id)
+      redirect_to quiz_challenger_question_path(@quiz.id, challenger.id, @question.id)
     else
       flash[:notice] = "ざんねん！"
-      redirect_to quiz_challenger_question_path(@quiz.id, @challenger.id, @question.id)
+      redirect_to quiz_challenger_question_path(@quiz.id, challenger.id, @question.id)
     end
   end
 
