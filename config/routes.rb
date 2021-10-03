f3bbb0179b65d5647a31c675ca88af2216901e15
ykemoemo <ykemoemo@gmail.com>
2021-10-03 20:14:56 +0900

    概要ページのリンクを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'quizzes#new'
 
+  # サービス概要
+  resources :helps, only: [:index]
+
   # 問題作成
   resources :quizzes, except: [:edit, :update, :destroy] do
     resources :questions, except: [:new, :edit, :update] do
